407a556d6bacfd31e29f68bf27190c75fd34cc42
vishwa200214@gmail.com <vishwa200214@gmail.co>
2023-08-24 12:12:02 +0530

    first commit

---
+++ project/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <marquee>Welcome to web navigator</marquee>
+        <a
+          className="App-link"
+          href="https://github.com/vishwa-cyber-alt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github profile
+        </a>
+
+  <a
+          className="App-link"
+          href="https://leetcode.com/VishwaV/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leetcode profile
+        </a>
+<a
+          className="App-link"
+          href="https://youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Youtube
+        </a>
+<a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google
+        </a>
+
+<a
+	className="App-link"
+	href="https://www.quora.com"
+>Quora</a>
+
+<a
+	className="App-link"
+	href="https://www.stackoverflow.com"
+>StackOverFlow</a>
+<a
+	className="App-link"
+	href="https://www.facebook.com"
+>Facebook meta</a>
+<p>Coded by Vishwa V</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
